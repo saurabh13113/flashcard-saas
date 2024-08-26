4da139c81b8592859062656fa98ee218dc41c989
Saurabh Nair <107759922+saurabh13113@users.noreply.github.com>
2024-08-26 13:51:15 +0400

    Update page.js

--- app/flashcard/page.js
+++ app/flashcard/page.js
@@ -61,63 +61,62 @@ export default function Flashcard() {
     <Container maxWidth="100%">
       <Grid container spacing={3} sx={{ mt: 4 }}>
         {flashcards.map((flashcard, index) => (
-           // eslint-disable-next-line react/jsx-key
-          <Grid item xs={12} sm={6} md={4} key={index}>
-            <Card>
-              <CardActionArea
-                onClick={() => {
-                  handleCardClick(index);
-                }}
-              >
-                <CardContent>
-                  <Box
-                    sx={{
-                      perspective: "1000px",
-                      "& > div": {
-                        transition: "transform 0.6s",
-                        transformStyle: "preserve-3d",
-                        position: "relative",
-                        width: "100%",
-                        height: "200px",
-                        boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
-                        transform: flipped[index]
-                          ? "rotateY(180deg)"
-                          : "rotateY(0deg)",
-                      },
-                      "& > div > div": {
-                        position: "absolute",
-                        width: "100%",
-                        height: "100%",
-                        backfaceVisibility: "hidden",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        padding: 2,
-                        boxSizing: "border-box",
-                      },
-                      "& > div > div:nth-of-type(2)": {
-                        transform: "rotateY(180deg)",
-                      },
-                    }}
-                  >
+        <Grid item xs={12} sm={6} md={4} key={flashcard.id || index}>
+          <Card>
+            <CardActionArea
+              onClick={() => {
+                handleCardClick(index);
+              }}
+            >
+              <CardContent>
+                <Box
+                  sx={{
+                    perspective: "1000px",
+                    "& > div": {
+                      transition: "transform 0.6s",
+                      transformStyle: "preserve-3d",
+                      position: "relative",
+                      width: "100%",
+                      height: "200px",
+                      boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+                      transform: flipped[index]
+                        ? "rotateY(180deg)"
+                        : "rotateY(0deg)",
+                    },
+                    "& > div > div": {
+                      position: "absolute",
+                      width: "100%",
+                      height: "100%",
+                      backfaceVisibility: "hidden",
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      padding: 2,
+                      boxSizing: "border-box",
+                    },
+                    "& > div > div:nth-of-type(2)": {
+                      transform: "rotateY(180deg)",
+                    },
+                  }}
+                >
+                  <div>
                     <div>
-                      <div>
-                        <Typography variant="h5" component="div">
-                          {flashcard.front}
-                        </Typography>
-                      </div>
-                      <div>
-                        <Typography variant="h5" component="div">
-                          {flashcard.back}
-                        </Typography>
-                      </div>
+                      <Typography variant="h5" component="div">
+                        {flashcard.front}
+                      </Typography>
                     </div>
-                  </Box>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Grid>
-        ))}
+                    <div>
+                      <Typography variant="h5" component="div">
+                        {flashcard.back}
+                      </Typography>
+                    </div>
+                  </div>
+                </Box>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>
+      ))}
       </Grid>
     </Container>
   );
